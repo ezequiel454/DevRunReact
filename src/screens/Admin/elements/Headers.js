c477d144b0ee7322619c946beeb4d40bc28bb116
ezequiel454 <emn@cin.ufpe.br>
2018-09-24 02:57:45 -0300

    init

---
+++ src/screens/Admin/elements/Headers.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import ActionCreators from '../../../redux/actionCreators'
+import { Link } from 'react-router-dom'
+
+import { Menu, Dropdown, Image } from 'semantic-ui-react'
+
+const Header = props => {
+  return (
+    <Menu>
+      <Menu.Item as={Link} to='/'><Image src={'/logo.png'} size='small'/></Menu.Item>
+      <Menu.Item as={Link} to='/admin'>Home</Menu.Item>
+      <Menu.Item as={Link} to='/admin/users'>Usuários</Menu.Item>
+      <Menu.Item as={Link} to='/admin/runs'>Runs</Menu.Item>
+      <Menu.Menu position='right'>
+        <Dropdown item text={props.auth.user.name}>
+          <Dropdown.Menu>
+            { props.auth.user.role === 'admin' && <Dropdown.Item as={Link} to='/restrito'>Mode: user</Dropdown.Item> }
+            <Dropdown.Item>Minha conta</Dropdown.Item>
+            <Dropdown.Item>Alterar senha</Dropdown.Item>
+            <Dropdown.Item onClick={props.logout}>Sair</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Menu>
+    </Menu>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signin: (email, senha) => dispatch(ActionCreators.signinRequest(email, senha)),
+    logout: () => dispatch(ActionCreators.destroyAuthRequest())
+
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
