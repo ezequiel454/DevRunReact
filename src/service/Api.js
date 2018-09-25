d072bb2bc71ff5bb6ed00977c7f0a7e085747998
ezequiel454 <ezequiel454@gmail.com>
2018-09-24 23:24:04 -0300

    :zap: Injection Dependency

---
+++ src/service/Api.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+const Api = base => {
+  const client = axios.create({
+    baseURL: base
+  })
+
+  const token = localStorage.getItem('token')
+  const get = endpoint => client.get(endpoint, {
+    headers:{
+      Authorization: 'Bearer '+ token
+    }
+  })
+
+  const remove = endpoint => client.delete(endpoint, {
+    headers:{
+      Authorization: 'Bearer '+ token
+    }
+  })
+
+  const update = (endpoint, data) => client.patch(endpoint, data, {
+    headers:{
+      Authorization: 'Bearer '+ token
+    }
+  })
+  
+  const create = (endpoint, data) => client.post(endpoint, data, {
+    headers:{
+      Authorization: 'Bearer '+ token
+    }
+  })
+
+  return {
+    getUser: id => get(`/users/${id}`),
+    getUsers: () => get(`/users`),
+    removeUser: id => remove(`/users/${id}`),
+    updateUser: data => update(`/users/${data.id}`, data),
+
+    removeRun: id => remove(`/runs/${id}`),
+    getRuns: admin => get(`/runs/${admin}`),
+    createRun: data => create(`/runs`, data)
+
+  }
+}
+
+export default Api
