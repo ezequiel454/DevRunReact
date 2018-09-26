c04f7b749054580bbeb798cfc7ea6e903c6377c8
ezequiel454 <emn@cin.ufpe.br>
2018-09-26 00:45:05 -0300

    :zap: Dependency Injection Finish

--- src/redux/sagas/auth.js
+++ src/redux/sagas/auth.js
@@ -1,19 +1,11 @@
-import axios from 'axios'
 import jwtDecode from 'jwt-decode'
 
-import { put } from 'redux-saga/effects'
+import { put, call } from 'redux-saga/effects'
 import ActionCreators from '../actionCreators'
 
-/*
-axiosaxios.create({
-  baseURL: 'http://localhost:3001'
-});
-*/
-
-export function* login (action) {
-  console.log('login', action)
-  let token = localStorage.getItem('token')
-  const login = yield axios.post('http://localhost:3001/users/login', {
+export const login = ({ api }) => function* (action) {
+  let token = ''
+  const login = yield call(api.login, {
     email: action.email,
     passwd: action.passwd
   })
@@ -22,34 +14,25 @@ export function* login (action) {
     localStorage.setItem('token', token)
 
     const user = jwtDecode(token)
-    //localStorage.setItem('user', user)
     yield put(ActionCreators.signinSuccess(user))
   } else {
     yield put(ActionCreators.signinFailure(login.data.message))
   }
 }
 
-export function* updateProfile(action){
-  //console.log(action.user)
-  const token = localStorage.getItem('token')
+export const updateProfile = ({ api }) => function* (action){
   const userToSave = {
     ...action.user
   }
-  yield axios.patch(`http://localhost:3001/users/${action.user.id}`, userToSave, {
-    headers:{
-      Authorization: 'Bearer '+ token
-    }
-  })
+  yield call (api.updateUser, userToSave)
   yield put(ActionCreators.updateProfileSuccess(userToSave))
 }
 
-export function* createProfile(action){
+export const createProfile = ({ api }) => function* (action){
   const userToSave = {
     ...action.user
-    //unit: action.user.unit,
-    //timezone: action.user.timezone
   }
-  const user = yield axios.post(`http://localhost:3001/users`, userToSave)
+  const user = yield call(api.createUser, userToSave)
   if(user && user.data && user.data.error){
     yield put(ActionCreators.createProfileFailure(user.data.message))
   }else{
@@ -58,16 +41,12 @@ export function* createProfile(action){
   }
 }
 
-export function* auth () {
+export const auth = ({ api }) => function* () {
   const token = localStorage.getItem('token')
   if (token) {
     try {
       //const user = jwtDecode(token)
-      const user = yield axios.get('http://localhost:3001/users/me',{
-        headers:{
-          Authorization: 'Bearer '+ token
-        }
-      })
+      const user = yield call(api.getUser, 'me')
       yield put(ActionCreators.authSuccess(user.data))
     } catch (err) {
       yield put(ActionCreators.authFailure('invalid token'))
